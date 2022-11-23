9c4d9f7483929c3e1b7e10bc7e7001a79a384304
Brian Nganga <briannganga70@gmail.com>
2022-11-24 00:06:54 +0300

    added more columns

--- practice.py
+++ practice.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-column = ["Bray","Batman","Spongebob"]
-titled_column = {"name": column}
-data = pd.DataFrame(titled_column)
+column = ["Bray","Batman","Spongebob"] #list
+titled_columns = {"Name": column,
+                 "Height": [1.7, 1.9, 0.25],
+                 "Weight": [62, 100, 1] }
+data = pd.DataFrame(titled_columns)
 print(data)
