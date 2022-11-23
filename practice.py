9b82f3ed42117182d0ec61e63586f620d0d74524
Brian Nganga <briannganga70@gmail.com>
2022-11-24 00:12:16 +0300

    Selecting a column

--- practice.py
+++ practice.py
@@ -5,4 +5,6 @@ titled_columns = {"Name": column,
                  "Height": [1.7, 1.9, 0.25],
                  "Weight": [62, 100, 1] }
 data = pd.DataFrame(titled_columns)
+select_column = data["Weight"] # Selects entire column Weight
 print(data)
+print(select_column)
