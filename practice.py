8a8fb99ffdd100547c1d9ecf5221b9e9d67fb213
Brian Nganga <briannganga70@gmail.com>
2022-11-24 00:30:53 +0300

    refactor

--- practice.py
+++ practice.py
@@ -1,15 +1,19 @@
 import pandas as pd
 
-column = ["Bray","Batman","Spongebob"] #list
+#creating dataframe
+
+column = ["Bray","Batman","Spongebob"]          #list
 titled_columns = {"Name": column,
                  "Height": [1.7, 1.9, 0.25],
-                 "Weight": [62, 100, 1] }
+                 "Weight": [62, 100, 1] }       #Dictionary
 data = pd.DataFrame(titled_columns)
 
-select_column = data["Weight"] # Selects entire column Weight
-select_column1 = data["Weight"][1] # Selects index 1 in column Weight
+#selecting values from dataframe
 
-select_row = data.iloc[2] # select entire row at index 2
+select_column = data["Weight"]       # Selects entire column Weight
+select_column1 = data["Weight"][1]   # Selects index 1 in column Weight
+
+select_row = data.iloc[2]            # select entire row at index 2
 select_row2 = data.iloc[2]["Weight"] #selects a single value of weight from row at index 2
 
 print(data)
