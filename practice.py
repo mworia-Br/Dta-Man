77f9c2728b421d84a5d62f23f8f7a58a27d1460a
Brian Nganga <briannganga70@gmail.com>
2022-11-24 00:41:27 +0300

    Added Bmi(Data manipulate)

--- practice.py
+++ practice.py
@@ -16,6 +16,15 @@ select_column1 = data["Weight"][1]   # Selects index 1 in column Weight
 select_row = data.iloc[2]            # select entire row at index 2
 select_row2 = data.iloc[2]["Weight"] #selects a single value of weight from row at index 2
 
+#Manipulating dataframe values
+bmi = []
+# formulae is weight/(height**2)
+for i in range(len(data)):           # bmi calc logic
+    bmi_score = data["Weight"][i]/(data['Height'][i]**2)
+    bmi.append(bmi_score)
+
+data["bmi"] = bmi
+
 print(data)
 #print(select_column)
 #print(select_column1)
